3e60a8a8326f10a866e5a13c07e1edd2a12ae617
DustinDiazLopez <hunterghost526@gmail.com>
2021-07-10 17:17:10 -0500

    Added interest type, and refactored types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,3 +1,41 @@
 declare module '*.png';
 declare module '*.jpg';
 declare module 'react-native-pixel-perfect';
+import InterestInterface from './interests'
+
+export type Question = string;
+export type Answer = string;
+export type SecurityQuestion = [Question, Answer];
+
+type Interest = keyof InterestInterface;
+export default Interest;
+
+const hey: Interest = ''
+
+export interface User {
+  id: string,
+  email: string,
+  username: string,
+  password: string,
+  imageUri?: string,
+  securityQuestionOne: SecurityQuestion,
+  securityQuestionTwo: SecurityQuestion,
+  securityQuestionThree: SecurityQuestion,
+  interests: Interest[],
+  contacts: User[],
+  roomsId: string, // ?
+};
+
+export interface Message {
+  id: string,
+  user: User,
+  content: string,
+  createdAt: string,
+};
+
+export interface ChatRoom {
+  id: string,
+  users: User[],
+  messages: Message[],
+  lastMessage: Message,
+};
