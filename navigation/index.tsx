6c557c8e377992c1a3c8fe625c9a1e90296c95cd
DustinDiazLopez <hunterghost526@gmail.com>
2021-07-22 15:54:34 -0500

    removed button

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -83,7 +83,7 @@ function RootNavigator() {
                 marginTop: 10,
               }}
               >
-                <PressableIcon
+                {/* <PressableIcon
                   IconProvider={Feather}
                   props={{
                     name: 'info',
@@ -93,7 +93,7 @@ function RootNavigator() {
                   style={{
                     paddingRight: 15,
                   }}
-                />
+                /> */}
                 {/* <PressableIcon
                   IconProvider={AntDesign}
                   props={{
